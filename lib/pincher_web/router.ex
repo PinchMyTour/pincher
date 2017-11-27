8ef11a689ebe3632a17344b956ff47d87704e235
Junaid Farooq <ijunaidfarooq@gmail.com>
2017-11-27 10:16:00 +0500

    Separate routes.

--- lib/pincher_web/router.ex
+++ lib/pincher_web/router.ex
@@ -16,9 +16,10 @@ defmodule PinchWeb.Router do
   scope "/", PinchWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/", IndexController, :sign_in
+    get "/", PageController, :index
 
     get "/pinch/sign_up", IndexController, :sign_up
+    get "/pinch/sign_in", IndexController, :sign_in
   end
 
   # Other scopes may use custom stacks.
