c706f00f6f0298dc5049bc580846370bb0a76991
Junaid Farooq <ijunaidfarooq@gmail.com>
2017-12-01 12:04:38 +0500

    Close Bulma alerts on close click(ed).

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1,1 +1,11 @@
+var closeBulmaAlerts = function () {
+  $(".bulma-delete").on("click", function (){
+    console.log("Closing bulma cautions!");
+    $(".bulma-sign-errors").css("display", "none");
+  });
+};
 
+
+window.startJSApp = function() {
+  closeBulmaAlerts();
+};
