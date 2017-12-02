cdee0a969c98b9fa2d5f301afbaa971d45550111
Junaid Farooq <ijunaidfarooq@gmail.com>
2017-12-02 21:43:56 +0500

    Get current LatLang using navigator.

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -9,8 +9,20 @@ var pinchMeClicked = function () {
   $(".pinch-me").on("click", function () {
     console.log("Pinch clicked... Saving location.")
     $(this).css("background", "#5b97f7");
+    getCurrentLatLong();
   });
 };
+
+function showPosition(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  console.log(lat);
+  console.log(lng);
+}
+
+var getCurrentLatLong = function () {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
 
 window.startJSApp = function() {
   closeBulmaAlerts();
