3016ee70e45a9e8a85e5a53fe5ea928473268594
Junaid Farooq <ijunaidfarooq@gmail.com>
2017-12-03 06:56:09 +0500

    Make it available to Heroku.

---
+++ config/prod.secret.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :pincher, PinchWeb.Endpoint,
+  secret_key_base: "Mr/EKMnQnhZ5t6nxpqWhyB2qXZUJ13qMcEXUAB5WdKEB9oVKKze6/OTcqkF6ZAeA"
+
+# Configure your database
+config :pincher, Pinch.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
